13f57e5874d4f544cbe2d76e7753f5a7f5873697
Jeremy D. Gerdes <seakintruth@gmail.com>
2020-02-03 06:40:23 -0500

    Update replace_null.R

--- R/replace_null.R
+++ R/replace_null.R
@@ -3,7 +3,7 @@
 #' @param replace the data to replace with
 #' @return x data with all NULL values replaced
 #' @examples
-#' replace_null(c(NULL,-49:48,Inf))
+#' replace_null(c(NULL,-49:48,Inf),0)
 #' @export
 replace_null <-
 function (x, replace=0, ...) {
